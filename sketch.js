b9043e3601d564f23bf463dfb765cc8492858c74
AlveMonke <89086047+AlveMonke@users.noreply.github.com>
2021-12-08 16:42:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,31 +2,39 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async (e) => {
         let useLat = e.coords.latitude;
         let useLong = e.coords.longitude;
-        
-        document.querySelector("a-scene").innerHTML += `<a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: ${useLong}; latitude: ${useLat};" animation-mixer />`;
 
-//         let key = "AHxoFnjGGgGyfjAA9kaQdpC3ID2AZ9dG";
+        console.log(document.querySelector("a-scene"));
 
-//         console.log(useLat, useLong);
+        document.querySelector(
+            "a-scene"
+        ).innerHTML += `<a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: ${useLong}; latitude: ${useLat};" animation-mixer look-at="[gps-camera]"/>`;
 
-//         let places = await fetch(
-//             `https://api.tomtom.com/search/2/nearbySearch/.json?lat=${useLat}&lon=${useLong}&radius=2&key=${key}`
-//         );
-//         places = await places.json();
+        console.log(document.querySelector("a-scene"));
 
-//         let placesArr = [];
+        // console.log(useLat, useLong);
 
-//         places.results.forEach((e) => {
-//             placesArr.push({
-//                 name: e.poi.name,
-//                 lat: e.position.lat,
-//                 lon: e.position.lon,
-//             });
-//         });
+        // let key = "AHxoFnjGGgGyfjAA9kaQdpC3ID2AZ9dG";
 
-//         placesArr.forEach((e) => {
-//             document.body.innerHTML += `<a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: ${e.lon}; latitude: ${e.lat};" animation-mixer />`;
-//         });
+        // console.log(useLat, useLong);
+
+        // let places = await fetch(
+        //     `https://api.tomtom.com/search/2/nearbySearch/.json?lat=${useLat}&lon=${useLong}&radius=2&key=${key}`
+        // );
+        // places = await places.json();
+
+        // let placesArr = [];
+
+        // places.results.forEach((e) => {
+        //     placesArr.push({
+        //         name: e.poi.name,
+        //         lat: e.position.lat,
+        //         lon: e.position.lon,
+        //     });
+        // });
+
+        // placesArr.forEach((e) => {
+        //     document.body.innerHTML += `<a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: ${e.lon}; latitude: ${e.lat};" animation-mixer />`;
+        // });
     });
 } else {
     document.body.innerHTML =
